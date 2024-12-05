5110ba8c728ee0bb25e43365b060e83b00c30b58
ALOK <rawatalok.817300@gmail.com>
2024-12-05 11:10:52 +0000

    conversion of data type

---
+++ .devcontainer/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "@Alok8468"
+
+let booleanIsLoggedIn = boolean(isLogged)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "@Alok8468" => true
+
+let someNumber =84
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
